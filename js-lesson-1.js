628f2c309238fc9d55dc8c272533179afb4e62e6
Kostyantyn <kosta.rusin@gmail.com>
2023-05-30 18:10:31 +0200

    1

---
+++ js-lesson-1.js
@@ -0,0 +1,11 @@
+const makeGreeting = guestName => {
+    if (guestName === "" || guestName === undefined) {
+      return Promise.reject("Guest name must not be empty");
+    }
+  
+    return Promise.resolve(`Welcome ${guestName}`);
+  };
+  
+  makeGreeting("")
+    .then(greeting => console.log(greeting))
+    .catch(error => console.error(error));
