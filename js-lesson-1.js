4c4df5eab3005b5fecbc6ef31f46b2a73209eabf
Kostyantyn <kosta.rusin@gmail.com>
2023-06-04 23:17:48 +0200

    Update js-lesson-1.js

--- js-lesson-1.js
+++ js-lesson-1.js
@@ -1,11 +1,8 @@
-const makeGreeting = guestName => {
-    if (guestName === "" || guestName === undefined) {
-      return Promise.reject("Guest name must not be empty");
-    }
-  
-    return Promise.resolve(`Welcome ${guestName}`);
-  };
-  
-  makeGreeting("")
-    .then(greeting => console.log(greeting))
-    .catch(error => console.error(error));
+try {
+  const data = JSON.parse('{"username": "Mango"}');
+} catch (error) {
+  console.log(error.name); // "SyntaxError"
+  console.log(error.message); // "Unexpected token u in JSON at position 1"
+}
+
+console.log("✅ This is fine, we handled parse error in try...catch");
